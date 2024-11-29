330492843b2d8e2fbfd36bcddb37231c306dff61
Gyanis <781680696@qq.com>
2024-11-29 20:38:14 +0800

    线程池cached模式设计

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -3,13 +3,12 @@
 //
 #include "ThreadPool.h"
 
-#include <utility>
+unsigned int Thread::m_generateID = 0;
 
-
-const int TASK_MAX_THREADS = 1024;
-
+/**----------------------------ThreadPool----------------------------------------------*/
 ThreadPool::ThreadPool() : m_initThreadSize(4), m_taskSize(0), m_taskQueMAXThreadHold(TASK_MAX_THREADS),
-                           m_poolMode(PoolMode::MODE_FIXED) {
+                           m_poolMode(PoolMode::MODE_FIXED), isPoolRunning(false), m_idleThreadSize(0),
+                           MAXThreadSizeHold(THREAD_MAX_HOLD), m_curThreadSize(0) {
 
 }
 
@@ -17,15 +16,27 @@ ThreadPool::~ThreadPool() {
 
 }
 
-void ThreadPool::start(int initThreadSize) {
-    m_initThreadSize = initThreadSize;
-    for (int i = 0; i < m_initThreadSize; ++i)
-        m_threads.emplace_back(std::move(std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this))));
-    for (int i = 0; i < m_initThreadSize; ++i)
-        m_threads[i]->start();
+void ThreadPool::setThreadSizeThreadHold(int thread_hold) {
+    if (checkRunningState()) {
+        std::cout << "线程池已启动，无法修改！" << std::endl;
+        return;
+    }
+    if (m_poolMode == PoolMode::MODE_CACHED) {
+        MAXThreadSizeHold = thread_hold;
+    } else {
+        std::cout << "线程池模式固定，无法修改线程数量！" << std::endl;
+    }
+}
+
+bool ThreadPool::checkRunningState() const {
+    return isPoolRunning;
 }
 
 [[maybe_unused]] void ThreadPool::setMode(PoolMode mode) {
+    if (checkRunningState()) {
+        std::cout << "线程池已启动，无法更改模式！" << std::endl;
+        return;
+    }
     m_poolMode = mode;
 }
 
@@ -34,26 +45,72 @@ void ThreadPool::start(int initThreadSize) {
     m_taskQueMAXThreadHold = threadHold;
 }
 
+void ThreadPool::start(int initThreadSize) {
+    isPoolRunning = true;
 
-void ThreadPool::submitTask(std::shared_ptr<Task> sp) {
+    m_initThreadSize = initThreadSize;
+    m_curThreadSize = initThreadSize;
+    for (int i = 0; i < m_initThreadSize; ++i) {
+        auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this, std::placeholders::_1));
+        int threadID = ptr->getID();
+        m_threads.emplace(threadID, std::move(ptr));
+    }
+    for (int i = 0; i < m_initThreadSize; ++i) {
+        m_threads[i]->start();
+        m_idleThreadSize++;
+    }
+}
+
+Result ThreadPool::submitTask(std::shared_ptr<Task> sp) {
     std::unique_lock<std::mutex> lock(m_taskQueMex);
-    if (!m_notFull.wait_for(lock, std::chrono::seconds(1),
-                            [&]() -> bool { return m_taskQu.size() < m_taskQueMAXThreadHold; })) {
+    if (!m_notFull.wait_for(lock, std::chrono::seconds(2),
+                            [&]() -> bool { return m_taskSize < m_taskQueMAXThreadHold; })) {
         std::cerr << "task queue is full, submit task error!" << std::endl;
+        return Result(sp, false);
     }
     m_taskQu.emplace(sp);
     m_taskSize++;
     m_notEmpty.notify_all();
+    if (m_poolMode == PoolMode::MODE_CACHED && m_taskSize > m_idleThreadSize && m_curThreadSize < MAXThreadSizeHold) {
+        auto ptr = std::make_unique<Thread>(std::bind(&ThreadPool::threadFunc, this, std::placeholders::_1));
+        int threadID = ptr->getID();
+        m_threads.emplace(threadID, std::move(ptr));
+        m_curThreadSize++;
+        m_threads[std::make_unique<Thread>(
+                std::bind(&ThreadPool::threadFunc, this, std::placeholders::_1))->getID()]->start();
+        m_idleThreadSize++;
+    }
+    return Result(sp);
 }
 
-[[noreturn]] void ThreadPool::threadFunc() {
+void ThreadPool::threadFunc(int threadID) {
 
+    auto last_time = std::chrono::high_resolution_clock().now();
     std::shared_ptr<Task> task;
     for (;;) {
         {
             {
                 std::unique_lock<std::mutex> lock(m_taskQueMex);
-                m_notEmpty.wait_for(lock, std::chrono::seconds(1), [&]() -> bool { return !m_taskQu.empty(); });
+                std::cout << "tid: " << std::this_thread::get_id() << " 尝试获取任务..." << std::endl;
+                if (m_poolMode == PoolMode::MODE_CACHED) {
+                    while (m_taskQu.size() == 0) {
+                        if (std::cv_status::no_timeout == m_notEmpty.wait_for(lock, std::chrono::seconds(1))) {
+                            auto now_time = std::chrono::high_resolution_clock().now();
+                            auto dur = std::chrono::duration_cast<std::chrono::seconds>(now_time - last_time);
+                            if (dur.count() >= THREAD_MAX_IDLE_TIME && m_curThreadSize > m_initThreadSize) {
+                                m_threads.erase(threadID);
+                                m_curThreadSize--;
+                                m_idleThreadSize--;
+                                std::cout << "tid = " << std::this_thread::get_id() << " exit!" << std::endl;
+                                break;
+                            }
+                        }
+                    }
+                } else {
+                    m_notEmpty.wait(lock, [&]() -> bool { return !m_taskQu.empty(); });
+                }
+                m_idleThreadSize--;
+                std::cout << "tid: " << std::this_thread::get_id() << " 获取任务成功..." << std::endl;
                 task = m_taskQu.front();
                 m_taskQu.pop();
                 m_taskSize--;
@@ -63,23 +120,46 @@ void ThreadPool::submitTask(std::shared_ptr<Task> sp) {
                 m_notFull.notify_all();
             }
             if (task != nullptr) {
-                task->run();
+                task->exec();
             }
+            m_idleThreadSize++;
+            last_time = std::chrono::high_resolution_clock().now();
         }
     }
 }
 
-
-/**Thread实现*/
-Thread::Thread(ThreadFunc func) : m_func(std::move(func)) {
-
+/**-----------------------------------------------Task----------------------------------------------*/
+void Task::exec() {
+    {
+        if (m_result != nullptr)
+            m_result->setVal(run());
+    }
 }
 
-void Thread::start() {
-    std::thread t(m_func);
-    t.detach();
+void Task::setResult(Result *res) {
+    {
+        this->m_result = res;
+    }
 }
 
-Thread::~Thread() {
+Task::Task() : m_result(nullptr) {}
 
+
+/**-----------------------------------------------Result--------------------------------------------------------*/
+Result::Result(std::shared_ptr<Task> task, bool isValid) : m_task(task), m_isValid(isValid) {
+    m_task->setResult(this);
 }
+
+void Result::setVal(Any any) {
+    this->m_any = std::move(any);
+    m_sem.post();
+}
+
+Any Result::get() {
+    if (!m_isValid)
+        return "";
+    m_sem.wait();
+    return std::move(m_any);
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
 #include <iostream>
 #include "ThreadPool.h"
 
+
+using uLong = unsigned long long;
+
 class MyTask : public Task {
 public:
-    void run() override {
-        std::cout << "begin threadFunc" << ", threadID: " << std::this_thread::get_id() << std::endl;
-        std::cout << "end threadFunc" << ", threadID: " << std::this_thread::get_id() << std::endl;
+    MyTask(uLong begin, uLong end) : m_begin(begin), m_end(end) {}
+
+    Any run() override {
+        std::cout << "tid:" << std::this_thread::get_id() << "     "
+                  << "begin!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        uLong sum = 0;
+        for (uLong i = m_begin; i <= m_end; i++)
+            sum += i;
+        std::cout << "tid:" << std::this_thread::get_id() << "    "
+                  << "end!" << std::endl;
+
+        return sum;
     }
+
+    ~MyTask() = default;
+
+private:
+    uLong m_begin;
+    uLong m_end;
 };
 
 
 int main() {
-    ThreadPool pool;
-    pool.start(4);
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    pool.submitTask(std::make_shared<MyTask>());
-    while (true) {}
+    {
+        ThreadPool pool;
+        pool.setMode(PoolMode::MODE_CACHED);
+        pool.start(4);
+        Result res1 = pool.submitTask(std::make_shared<MyTask>(0, 100000000));
+        Result res2 = pool.submitTask(std::make_shared<MyTask>(100000000, 200000000));
+        Result res3 = pool.submitTask(std::make_shared<MyTask>(300000000, 400000000));
+        uLong sum1 = res1.get().cast_<uLong>();
+        std::cout << sum1 << std::endl;
+        uLong sum2 = res2.get().cast_<uLong>();
+        std::cout << sum2 << std::endl;
+        uLong sum3 = res3.get().cast_<uLong>();
+        std::cout << sum3 << std::endl;
+        std::cout << "sum = " << sum1 + sum2 + sum3 << std::endl;
+    }
+    getchar();
 }
